05aad033be8cf6c4e0c9f8a8c4b50a00452e8222
Dmitry Arsenyev <arsenyevdm1@gmail.com>
2023-11-18 15:15:11 +0400

    fresh view

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
-'use strict';
+"use strict"
 
-const panelText = document.querySelector('.panel').innerText;
-console.log(panelText);
-document.querySelector('.panel').innerText = 'New text';
-document.querySelector('.input').value = 'Text';
+function submitForm() {
+  const input = document.querySelector(".input").value
+  if (!input) {
+    return
+  }
+  document.querySelector(".panel").innerText = input
+  document.querySelector(".input").value = ""
+  const textString = JSON.stringify({
+    text: input,
+  })
+  localStorage.setItem("text", textString)
+}
+
+function inputChanged(e) {
+  if (e.code == "Enter") {
+    submitForm()
+  }
+}
+/*
+console.log(document.querySelectorAll(".one")[0].innerText)
+console.log(document.querySelectorAll(".one")[1].innerText)
+console.log(document.getElementById("two").innerText)
+console.log(document.getElementsByClassName(".one"))
+console.log(document.querySelector("[user-id='4']").innerText)
+
+
+const panelText = "Панель"
+const panelClass = "button"
+const newElement = document.createElement("button")
+newElement.setAttribute("user-id", 1)
+newElement.classList.add("panel")
+newElement.innerHTML = `<button class="${panelClass}">${panelText}</button>`
+document.querySelector(".test").appendChild(newElement)
+
+
+localStorage.setItem("token", "sffgds")
+localStorage.setItem("token1", 1)
+localStorage.setItem("token1", true)
+const token1 = localStorage.getItem("token1")
+console.log(typeof token1)
+*/
+
+const obj = JSON.parse('{ "a": 1 }')
+console.log(obj.a)
+const str = JSON.stringify(obj)
+console.log(str)
